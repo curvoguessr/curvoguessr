01d2f7d5a8b2505c0aa9d8c41f1e548f2b333e2d
go-loop <goloop271@gmail.com>
2026-08-07 16:00:15 +0530

    Changing canvas size as per plane and drawingplane

--- js/config.js
+++ js/config.js
@@ -1,16 +1,34 @@
-const subxunit = 20;
-const subyunit = 20;
+const plane = document.getElementById('plane');
+const drawingplane = document.getElementById('drawingplane')
+const canvas = document.getElementById("canvas");
+let vw = window.innerWidth/100;
+let vh = window.innerHeight/100;
+if(vw>1.1*vh){
+    vw = 1.1*vh;
+}
+if(vh>1.2*vw){
+    vh = 1.2*vw;
+}
+let subxunit = 2*vw;
+let subyunit = 2*vh;
 
-const xunit = 2*subxunit;
-const yunit = 2*subyunit;
+let xunit = 2*subxunit;
+let yunit = 2*subyunit;
 
-const cw = 800;
-const ch = 1000;
+let cw = 80*vw;
+let ch = 80*vh;
 
-const origin = {
+let origin = {
     x: cw/2,
     y: ch/2
 }
+canvas.style.width = cw + "px";
+canvas.style.height = ch + "px";
+plane.width = cw;
+drawingplane.width = plane.width;
+plane.height = ch;
+drawingplane.height = plane.height;
+
 
 function Function_x(t) {
     return t;
@@ -20,10 +38,10 @@ function Function_y(t) {
 }
 
 const range = {
-    xl : -20,
-    xr : 20,
-    yl : -20,
-    yr: 20
+    xl : -10,
+    xr : 10,
+    yl : -10,
+    yr: 10
 };
 
 const pRange = {
