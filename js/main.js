02cc0bf7796965048444b4bbcb492965a9365d1d
go-loop <goloop271@gmail.com>
2026-08-07 07:56:00 +0530

    Changing plane as per browser size, and adding overlap for plane and drawing plane

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ if(colourmode == "dark"){
     if(mode == "eraser"){
         DrawingPlane.style.cursor = "url('images/erasercursordark.png') 16 16 auto";
     }
-    DrawAxis();
+    DrawAxis(subxunit,xunit,cw,subyunit,yunit,ch);
 }
 else {
     defaultcolour1 = "#d6d6d6";
@@ -32,8 +32,94 @@ else {
     if(mode == "eraser"){
         DrawingPlane.style.cursor = "url('images/erasercursorlight.png') 16 16 auto";
     }
-    DrawAxis();
+    DrawAxis(subxunit,xunit,cw,subyunit,yunit,ch);
 }
+function overlap(){
+    const plane = document.getElementById("plane");
+    const drawingplane = document.getElementById("drawingplane");
+    const rect = plane.getBoundingClientRect();
+    drawingplane.style.position = "absolute";
+    drawingplane.style.top = rect.top + "px";
+    drawingplane.style.left = rect.left + "px";
+}
+window.addEventListener("load",()=>{
+    overlap();
+});
+
+window.addEventListener("resize",()=>{
+    const plane = document.getElementById('plane');
+    const drawingplane = document.getElementById('drawingplane');
+    const context1 = plane.getContext("2d");
+    const context = drawingplane.getContext("2d");
+    context1.resetTransform();
+    context.resetTransform();
+    context.clearRect(0,0,plane.width,plane.height);
+
+    vw = window.innerWidth/100;
+    vh = window.innerHeight/100;
+    if(vw>1.238*vh){
+       vw = 1.238*vh;
+    }
+    subxunit = 2*vw;
+    subyunit = 2*vh;
+
+    xunit = 2*subxunit;
+    yunit = 2*subyunit;
+
+    cw = 80*vw;
+    ch = 80*vh;
+    origin = {
+        x: cw/2,
+        y: ch/2
+    }
+    plane.width = cw;
+    drawingplane.width = cw;
+    plane.height = ch;
+    drawingplane.height = ch;
+    overlap();
+    // TransformCanvas(context,cw,ch);
+    DrawAxis(subxunit,xunit,cw,subyunit,yunit,ch);
+    context.globalCompositeOperation = "source-over";
+    context.lineWidth = 3;
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.strokeStyle = defaultcolour2;
+    for(let i = 0; i < unflattened.length; i++){
+        context.beginPath();
+        for(let j = 0; j < unflattened[i][0].length; j++){
+            if(j == 0){
+                context.moveTo(denormalise(destandardize(unflattened[i][0][0])).x, denormalise(destandardize(unflattened[i][0][0])).y);
+            }
+            if(1<=j && j<2){
+                context.lineTo(denormalise(destandardize(unflattened[i][0][j])).x, denormalise(destandardize(unflattened[i][0][j])).y);
+            }
+            if(j>=3){
+                CatRomGraph(denormalise(destandardize(unflattened[i][0][j-3])),denormalise(destandardize(unflattened[i][0][j-2])),denormalise(destandardize(unflattened[i][0][j-1])),denormalise(destandardize(unflattened[i][0][j])),0.05,context);
+            }
+        }
+        context.stroke();
+    }
+    context.globalCompositeOperation = "destination-out";
+    context.lineWidth = 30;
+    context.lineCap = "round";
+    context.lineJoin = "round";    
+    for(let i = 0; i < eraserunflattened.length; i++){
+        context.beginPath();
+        for(let j = 0; j < eraserunflattened[i][0].length; j++){
+            if(j == 0){
+                context.moveTo(denormalise(destandardize(eraserunflattened[i][0][0])).x,denormalise(destandardize(eraserunflattened[i][0][0])).y);
+            }
+            if(1<=j && j<2){
+                context.lineTo(denormalise(destandardize(eraserunflattened[i][0][j])).x, denormalise(destandardize(eraserunflattened[i][0][j])).y);
+            }
+            if(j>=3){
+                CatRomGraph(denormalise(destandardize(eraserunflattened[i][0][j-3])),denormalise(destandardize(eraserunflattened[i][0][j-2])),denormalise(destandardize(eraserunflattened[i][0][j-1])),denormalise(destandardize(eraserunflattened[i][0][j])),0.05,context);
+            }
+        }
+        context.stroke();
+    }
+});
+
 InitializeError();
 
 UserDrawing();
