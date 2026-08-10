7bb333ed2b9816929f15649f2b9975c46f2dd804
go-loop <goloop271@gmail.com>
2026-08-10 08:44:46 +0530

    Render equation in level10 config

--- levels/level10/config.js
+++ levels/level10/config.js
@@ -2,8 +2,10 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
+katex.render("\\ln(y) = \\sin(x)", equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
