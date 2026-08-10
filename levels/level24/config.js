7505ac70967c2c057aa7124dd3fefdfbecab96cb
go-loop <goloop271@gmail.com>
2026-08-10 09:00:40 +0530

    Render equation on canvas using KaTeX in level 24

--- levels/level24/config.js
+++ levels/level24/config.js
@@ -1,8 +1,10 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
+katex.render("y = 5\\sum_{k=1}^{12}\\frac{\\sin\\left(\\left(2k+1\\right)\\cdot x\\right)}{2k+1}", equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
