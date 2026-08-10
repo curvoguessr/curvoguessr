8a3434d7c0d927d7462154121f5317badf3cb458
go-loop <goloop271@gmail.com>
2026-08-10 08:42:13 +0530

    Render equation in level 8 configuration

--- levels/level8/config.js
+++ levels/level8/config.js
@@ -1,8 +1,10 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
+katex.render("y = x - \\frac{1}{x^2}", equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
