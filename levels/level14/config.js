75a70710fd09c76b6a2b7b902c416c9e94191aa6
go-loop <goloop271@gmail.com>
2026-08-10 08:48:41 +0530

    Render KaTeX equation in level14 config

--- levels/level14/config.js
+++ levels/level14/config.js
@@ -1,8 +1,10 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
+katex.render("y+e = e^{\\sec\\left(\\frac{3x}{4}\\right)}", equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
