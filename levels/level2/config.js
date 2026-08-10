c3f94570037b55d87dee2675517e8ee305232c41
go-loop <goloop271@gmail.com>
2026-08-10 08:35:59 +0530

    Add KaTeX rendering to level2 config

--- levels/level2/config.js
+++ levels/level2/config.js
@@ -1,8 +1,10 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
+katex.render("\\sqrt{|x|}+\\sqrt{|y|} = 3", equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
@@ -11,7 +13,7 @@ if(vh>1.2*vw){
 }
 let scale = 2;
 let subxunit = scale*vw;
-let subyunit = scale*vh;
+let subyunit = scale*vh
 
 let xunit = 2*subxunit;
 let yunit = 2*subyunit;
