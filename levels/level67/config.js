eecd75aa79b72a3eb35f1913dc454d4bfbbe7b87
go-loop <goloop271@gmail.com>
2026-08-20 23:17:02 +0530

    Update equation rendering and adjust font size

--- levels/level67/config.js
+++ levels/level67/config.js
@@ -1,26 +1,42 @@
 const plane = document.getElementById('plane');
 const drawingplane = document.getElementById('drawingplane')
 const canvas = document.getElementById("canvas");
+const equation = document.getElementById('equation');
 let vw = window.innerWidth/100;
 let vh = window.innerHeight/100;
-let equations = [];
-for (let i = 0; i < 14; i++) {
-    equations.push(document.getElementById('equation'+i));
+
+// katex.render("\\frac{x^{2}}{6.6^{2}}+\\frac{(y+0.6)^{2}}{5.7^{2}}=1",equations);
+// katex.render("y = 6.75-6.6\\left(\\frac{x}{3}+1.18\\right)^{2}+1.65\\left(\\frac{x}{3}+1.18\\right), -5.4 \\le x \\le -1.5", equations[1]);
+// katex.render("y = 6.75 - 6.6\\left(\\frac{x}{3}-1.18\\right)^2 - 1.65\\left(\\frac{x}{3}-1.18\\right), \\quad 1.5 \\le x \\le 5.4",equations[2]);
+// katex.render("y = 0.96 - 2.25\\left(\\frac{x}{3}+0.85\\right)^2, \\quad -4.05 \\le x \\le -1.05",equations[3]);
+// katex.render("y = 0.96 - 2.25\\left(\\frac{x}{3}-0.85\\right)^2, \\quad 1.05 \\le x \\le 4.05",equations[4]);
+// katex.render("y = -1.65 - 2.4\\sqrt{0.25^2-\\left(\\frac{x}{3}\\right)^2}, \\quad -0.75 \\le x \\le 0.75",equations[5]);
+// katex.render("y = -2.16 - 0.96\\sqrt{1-\\left(\\frac{\\frac{x}{3}+0.45}{0.45}\\right)^2}, \\quad -2.7 \\le x \\le 0",equations[6]);
+// katex.render("y = -2.16 - 0.96\\sqrt{1-\\left(\\frac{\\frac{x}{3}-0.45}{0.45}\\right)^2}, \\quad 0 \\le x \\le 2.7",equations[7]);
+// katex.render("y = 0.15 + 0.15\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[8]);
+// katex.render("y = -1.14 - 0.06\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[9]);
+// katex.render("y = -2.34 - 0.24\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[10]);
+// katex.render("y = 0.15 + 0.15\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[11]);
+// katex.render("y = -1.14 - 0.06\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[12]);
+//katex.render("y = -2.34 - 0.24\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[13]);
+let equations=[
+    "\\begin{aligned} y &= -0.6\\pm5.7\\sqrt{1-\\frac{x^2}{6.6^2}} \\\\",
+    "y &= 6.75 - 6.6\\left(\\frac{\\left|x\\right|}{3}-1.18\\right)^2 - 1.65\\left(\\frac{\\left|x\\right|}{3}-1.18\\right), \\quad 1.5 \\le \\left|x\\right| \\le 5.4 \\\\",
+    "y &= 0.96 - 2.25\\left(\\frac{\\left|x\\right|}{3}-0.85\\right)^2, \\quad 1.05 \\le \\left|x\\right| \\le 4.05 \\\\",
+    "y &= -1.65 - 2.4\\sqrt{0.25^2-\\left(\\frac{x}{3}\\right)^2}, \\quad -0.75 \\le x \\le 0.75 \\\\",
+    "y &= -2.16 - 0.96\\sqrt{1-\\left(\\frac{\\left|x\\right|-1.35}{1.35}\\right)^2}, \\quad 0 \\le \\left|x\\right| \\le 2.7 \\\\",
+    "y &= 0.15 + 0.15\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2 \\\\",
+    "y &= -1.14 - 0.06\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2 \\\\",
+    "y &= -2.34 - 0.24\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2 \\\\",
+    "y &= 0.15 + 0.15\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9 \\\\",
+    "y &= -1.14 - 0.06\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9 \\\\",
+    "y &= -2.34 - 0.24\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9 \\end{aligned}"
+]
+let allequations = "";
+for (let i = 0; i < 11; i++) {
+    allequations += equations[i];
 }
-katex.render("\\frac{x^{2}}{6.6^{2}}+\\frac{(y+0.6)^{2}}{5.7^{2}}=1",equations[0]);
-katex.render("y = 6.75-6.6\\left(\\frac{x}{3}+1.18\\right)^{2}+1.65\\left(\\frac{x}{3}+1.18\\right), -5.4 \\le x \\le -1.5", equations[1]);
-katex.render("y = 6.75 - 6.6\\left(\\frac{x}{3}-1.18\\right)^2 - 1.65\\left(\\frac{x}{3}-1.18\\right), \\quad 1.5 \\le x \\le 5.4",equations[2]);
-katex.render("y = 0.96 - 2.25\\left(\\frac{x}{3}+0.85\\right)^2, \\quad -4.05 \\le x \\le -1.05",equations[3]);
-katex.render("y = 0.96 - 2.25\\left(\\frac{x}{3}-0.85\\right)^2, \\quad 1.05 \\le x \\le 4.05",equations[4]);
-katex.render("y = -1.65 - 2.4\\sqrt{0.25^2-\\left(\\frac{x}{3}\\right)^2}, \\quad -0.75 \\le x \\le 0.75",equations[5]);
-katex.render("y = -2.16 - 0.96\\sqrt{1-\\left(\\frac{\\frac{x}{3}+0.45}{0.45}\\right)^2}, \\quad -2.7 \\le x \\le 0",equations[6]);
-katex.render("y = -2.16 - 0.96\\sqrt{1-\\left(\\frac{\\frac{x}{3}-0.45}{0.45}\\right)^2}, \\quad 0 \\le x \\le 2.7",equations[7]);
-katex.render("y = 0.15 + 0.15\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[8]);
-katex.render("y = -1.14 - 0.06\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[9]);
-katex.render("y = -2.34 - 0.24\\left(\\frac{x}{3}+1.4\\right)^2, \\quad -9 \\le x \\le -4.2",equations[10]);
-katex.render("y = 0.15 + 0.15\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[11]);
-katex.render("y = -1.14 - 0.06\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[12]);
-katex.render("y = -2.34 - 0.24\\left(\\frac{x}{3}-1.4\\right)^2, \\quad 4.2 \\le x \\le 9",equations[13]);
+katex.render(allequations,equation);
 if(vw>1.1*vh){
     vw = 1.1*vh;
 }
@@ -219,3 +235,41 @@ let range= {
     yl: -10,
     yr: 10
 }
+if(window.matchMedia("(min-width: 769px)").matches){
+    const undobutton2 = document.getElementById("undobutton");
+    const equation2 = document.getElementById("equation");
+    const darkmodebutton2 = document.getElementById("darkbutton");
+    equation2.style.fontSize = "2vh";
+    let equationxcoordleft = equation2.getBoundingClientRect().left;
+    let equationxcoordright = equation2.getBoundingClientRect().right;
+    let undobuttonxcoord1 = undobutton2.getBoundingClientRect().left + undobutton2.getBoundingClientRect().width;
+    let darkbuttonxcoord = darkmodebutton2.getBoundingClientRect().right + darkmodebutton2.getBoundingClientRect().width;
+    while(equationxcoordleft <= undobuttonxcoord1 + 10 || equationxcoordright >= darkbuttonxcoord - 10){
+        let currentfontsize = parseFloat(equation2.style.fontSize);
+        equation2.style.fontSize = (currentfontsize*0.95) + "vh";
+        darkbuttonxcoord = darkmodebutton2.getBoundingClientRect().right + darkmodebutton2.getBoundingClientRect().width;
+        equationxcoordleft = equation2.getBoundingClientRect().left;
+        equationxcoordright = equation2.getBoundingClientRect().right;
+        undobuttonxcoord1 = undobutton2.getBoundingClientRect().left + undobutton2.getBoundingClientRect().width;
+    }
+}
+window.addEventListener("resize",()=>{
+    if(window.matchMedia("(min-width: 769px)").matches){
+         const undobutton2 = document.getElementById("undobutton");
+        const equation2 = document.getElementById("equation");
+        const darkmodebutton2 = document.getElementById("darkbutton");
+        equation2.style.fontSize = "2vh";
+        let equationxcoordleft = equation2.getBoundingClientRect().left;
+        let equationxcoordright = equation2.getBoundingClientRect().right;
+        let undobuttonxcoord1 = undobutton2.getBoundingClientRect().left + undobutton2.getBoundingClientRect().width;
+        let darkbuttonxcoord = darkmodebutton2.getBoundingClientRect().right + darkmodebutton2.getBoundingClientRect().width;
+        while(equationxcoordleft <= undobuttonxcoord1 + 10 || equationxcoordright >= darkbuttonxcoord - 10){
+            let currentfontsize = parseFloat(equation2.style.fontSize);
+            equation2.style.fontSize = (currentfontsize*0.95) + "vh";
+            darkbuttonxcoord = darkmodebutton2.getBoundingClientRect().right + darkmodebutton2.getBoundingClientRect().width;
+            equationxcoordleft = equation2.getBoundingClientRect().left;
+            equationxcoordright = equation2.getBoundingClientRect().right;
+            undobuttonxcoord1 = undobutton2.getBoundingClientRect().left + undobutton2.getBoundingClientRect().width;
+        }
+    }
+});
