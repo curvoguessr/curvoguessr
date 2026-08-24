13a328c91b5bb02dce81bf0d6b7ee52f267a22e9
go-loop <goloop271@gmail.com>
2026-08-24 05:45:43 +0530

    Update error.js

--- js/error.js
+++ js/error.js
@@ -1,150 +1,169 @@
-class KD {
-    constructor(point, axis) {
-        this.point = point;
-        this.axis = axis;
-        this.left = null;
-        this.right = null;
-    }
-    static Build(points, depth = 0) {
-        if (points.length == 0) {
-            return null;
-        }
-        let axis = depth%2;
-        let mid = Math.floor(points.length/2);
-        points.sort((a, b) => axis == 0 ? a.x-b.x : a.y - b.y);
-        const node = new KD(points[mid], axis);
-        node.left = KD.Build(points.slice(0, mid), depth+1);
-        node.right = KD.Build(points.slice(mid+1), depth+1);
-        return node;
-    }
-    static Nearest(cur, query, best) {
-        if (cur == null) return;
-        let d = EuclideanDist(query, cur.point);
-        if (best.dist > d) {
-            best.dist = d;
-            best.point = cur.point;
-        }
-        let near, far;
-        if (cur.axis == 0) {
-            if (cur.point.x >= query.x) {
-                near = cur.left;
-                far = cur.right;
-            }
-            else {
-                near = cur.right;
-                far = cur.left;
-            }
-        }
-        else {
-            if (cur.point.y >= query.y) {
-                near = cur.left;
-                far = cur.right;
-            }
-            else {
-                near = cur.right;
-                far = cur.left;
-            }
-        }
-        KD.Nearest(near, query, best);
-        let delta = cur.axis == 0 ? query.x - cur.point.x : query.y - cur.point.y;
-        if (Math.abs(delta) < best.dist) {
-            KD.Nearest(far, query, best);
+function GetEquidistantPoints(Function_x, Function_y, tl, tr, spacing, dt = 0.001) {
+    const points = [];
+    let t = tl;
+    let prev = {
+        x: Function_x(t),
+        y: Function_y(t)
+    };
+    points.push(prev);
+    let accum = 0;
+    while (t < tr) {
+        const nextT = Math.min(t + dt, tr);
+
+        const next = {
+            x: Function_x(nextT),
+            y: Function_y(nextT)
+        };
+
+        const dx = next.x - prev.x;
+        const dy = next.y - prev.y;
+        const d = EuclideanDist(prev, next);
+
+        if (accum + d >= spacing) {
+            const f = (spacing - accum) / d;
+
+            const point = {
+                x: prev.x + f * dx,
+                y: prev.y + f * dy
+            };
+
+            points.push(point);
+
+            prev = point;
+            accum = 0;
+
+            t -= dt * (1 - f);
+        } else {
+            accum += d;
+            prev = next;
+            t = nextT;
         }
     }
-};
-let rootActual = null;
-let rootUser = null;
+    const end = {
+        x: Function_x(tr),
+        y: Function_y(tr)
+    };
+
+    if (points.length === 0 || EuclideanDist(end, points[points.length-1]) > spacing * 0.5) {
+        points.push(end);
+    }
+
+    return points;
+}
+let accuracy;
 let actuallen = 0;
 let allActual = [];
-let distActual = 0;
+let actualUnflattened = [];
 function InitializeError() {
     for (let i = 0; i < Graph.length; i++) {
-        let prev = {x: -11, y: -11};
-        for (let t = Graph[i].pRange.l; t <= Graph[i].pRange.r; t += 0.005) {
-            let p = {x: Graph[i].Function_x(t), y: Graph[i].Function_y(t)};
-            if (p.x >= range.xl && p.x <= range.xr && p.y >= range.yl && p.y <= range.yr) {
-                allActual.push(p);
-                if (prev.x != -11) {
-                    distActual += EuclideanDist(prev, p);
-                }
-                prev = p;
-            }
-        }
+
+        let actual = GetEquidistantPoints(Graph[i].Function_x, Graph[i].Function_y, Graph[i].pRange.l, Graph[i].pRange.r, graphLen/500);
+        allActual.push(...actual);
     }
-    
     actuallen = allActual.length;
-    rootActual = KD.Build(allActual);
 }
-function resampleCatRom(mousecoord, spacing) {
-    if (mousecoord.length < 2) {
-        return mousecoord.map(p => denormalise(destandardize(p)));
-    }
-    let points = mousecoord.map(p => denormalise(destandardize(p)));
 
-    let dense = [];
-    const steps = 100;
-    for (let i = 3; i < points.length; i++) {
-        for (let j = 0; j < steps; j++) {
-            const t = j/steps;
-            dense.push(CatRom(points[i-3],points[i-2],points[i-1],points[i],t));
-        }
-    }
-    dense.push(points[points.length-1]);
-    let user = [dense[0]];
-    let acc = 0;
-    for (let i = 1; i < dense.length; i++) {
-        acc += EuclideanDist(dense[i-1],dense[i]);
-        if (acc >= spacing) {
-            user.push(dense[i]);
-            acc -= spacing;
-        }
-    }
-    const last = dense[dense.length-1];
-    if (user[user.length-1].x !== last.x || user[user.length-1].y !== last.y) {
-        user.push(last);
-    }
-    for (let i = 0; i < user.length; i++) {
-        user[i]=Convert(user[i]);
-    }
-    return user;
-}
+const worker = new Worker("../../js/worker1.js");
 function FindError(mousecoord, unflattened, colour) {
-    let distUser = 0;
-    for (let i = 0; i < unflattened.length; i++) {
-        for (let j = 1; j < unflattened[i].length; j++) {
-            let p1 = Convert(denormalise(destandardize(unflattened[i][j])));
-            let p2 = Convert(denormalise(destandardize(unflattened[i][j-1])));
-            distUser += EuclideanDist(p1,p2);
+    return new Promise((resolve) => {
+        worker.onmessage = (event) => {
+            accuracy = event.data;
+            
+            resolve();
+        };
+
+        worker.postMessage({
+            unflattened,
+            allActual,
+            actuallen,
+            cw,
+            ch,
+            xunit,
+            yunit
+        });
+    });
+}function GetEquidistantPoints(Function_x, Function_y, tl, tr, spacing, dt = 0.001) {
+    const points = [];
+    let t = tl;
+    let prev = {
+        x: Function_x(t),
+        y: Function_y(t)
+    };
+    points.push(prev);
+    let accum = 0;
+    while (t < tr) {
+        const nextT = Math.min(t + dt, tr);
+
+        const next = {
+            x: Function_x(nextT),
+            y: Function_y(nextT)
+        };
+
+        const dx = next.x - prev.x;
+        const dy = next.y - prev.y;
+        const d = EuclideanDist(prev, next);
+
+        if (accum + d >= spacing) {
+            const f = (spacing - accum) / d;
+
+            const point = {
+                x: prev.x + f * dx,
+                y: prev.y + f * dy
+            };
+
+            points.push(point);
+
+            prev = point;
+            accum = 0;
+
+            t -= dt * (1 - f);
+        } else {
+            accum += d;
+            prev = next;
+            t = nextT;
         }
     }
-    let user = [];
-    for (let i = 0; i < unflattened.length; i++) {
-        user.push(resampleCatRom(unflattened[i],0.1));
-    }
-    user = user.flat();
-    let errorUser = 0, errorActual = 0;
-    const len = user.length;
-    for (let i = 0; i < len; i++) {
-        let best = {dist: Infinity, point: null};
-        KD.Nearest(rootActual, user[i], best);
-        errorUser += best.dist;
-    }
-    let rootUser = KD.Build(user);
-    for (let i = 0; i < allActual.length; i++) {
-        let best = {dist: Infinity, point: null};
-        KD.Nearest(rootUser, allActual[i], best);
-        errorActual += best.dist;
-    }
-    errorUser /= len;
-    errorActual /= actuallen;
-    let error = (errorUser + errorActual)/2;
-    let errorDist = Math.max(distActual/distUser, distUser/distActual);
-    error *= errorDist;
+    const end = {
+        x: Function_x(tr),
+        y: Function_y(tr)
+    };
 
-    let accFactor = 0.2;
-    let accuracy = 100*Math.exp(-accFactor*error);
-    accuracy = Math.round(accuracy*100)/100;
-    let Accuracy = document.getElementById("accuracy");
-    Accuracy.style.color = colour;
-    Accuracy.innerHTML = "Accuracy: " + accuracy.toString() + "%";
+    if (points.length === 0 || EuclideanDist(end, points[points.length-1]) > spacing * 0.5) {
+        points.push(end);
+    }
+
+    return points;
+}
+let accuracy;
+let actuallen = 0;
+let allActual = [];
+let actualUnflattened = [];
+function InitializeError() {
+    for (let i = 0; i < Graph.length; i++) {
+
+        let actual = GetEquidistantPoints(Graph[i].Function_x, Graph[i].Function_y, Graph[i].pRange.l, Graph[i].pRange.r, graphLen/500);
+        allActual.push(...actual);
+    }
+    actuallen = allActual.length;
+}
+
+const worker = new Worker("../../js/worker1.js");
+function FindError(mousecoord, unflattened, colour) {
+    return new Promise((resolve) => {
+        worker.onmessage = (event) => {
+            accuracy = event.data;
+            
+            resolve();
+        };
+
+        worker.postMessage({
+            unflattened,
+            allActual,
+            actuallen,
+            cw,
+            ch,
+            xunit,
+            yunit
+        });
+    });
 }
